38e00f6992d85b82f9fbd39333bce426285f1f47
Sam Taub <staub91@gmail.com>
2019-02-17 21:53:25 -0500

    added API functionality

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendArray = [
+    {
+        name: "Tony",
+        photo: "",
+        scores: [
+            3, 4, 5, 1, 1, 4, 4, 4, 2, 1
+        ]
+    },
+    {
+        name: "George",
+        photo: "",
+        scores: [
+            2, 4, 3, 4, 4, 5, 1, 1, 3, 3
+        ]
+    },
+    {
+        name: "Michael",
+        photo: "",
+        scores: [
+            2, 5, 3, 3, 3, 4, 4, 3, 5, 5  
+        ]
+    },
+    {
+        name: "Pam",
+        photo: "",
+        scores: [
+            4, 3, 2, 3, 3, 2, 1, 1, 5, 2
+        ]
+    },
+    {
+        name: "Elaine",
+        photo: "",
+        scores: [
+            2, 4, 3, 3, 2, 1, 3, 3, 3, 1 
+        ]
+    }
+];
+
+module.exports = friendArray;
