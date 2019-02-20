835c687ec7bd63d0dc4dafde242361d899cb98ed
Sam Taub <staub91@gmail.com>
2019-02-20 17:02:03 -0500

    added match logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,35 @@
 var friendArray = [
     {
         name: "Tony",
-        photo: "",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Tony_Soprano.jpg/270px-Tony_Soprano.jpg",
         scores: [
             3, 4, 5, 1, 1, 4, 4, 4, 2, 1
         ]
     },
     {
         name: "George",
-        photo: "",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-seinfeld-jason-alexander.jpg",
         scores: [
             2, 4, 3, 4, 4, 5, 1, 1, 3, 3
         ]
     },
     {
         name: "Michael",
-        photo: "",
+        photo: "https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
         scores: [
             2, 5, 3, 3, 3, 4, 4, 3, 5, 5  
         ]
     },
     {
         name: "Pam",
-        photo: "",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-office-jenna-fischer.jpg",
         scores: [
             4, 3, 2, 3, 3, 2, 1, 1, 5, 2
         ]
     },
     {
         name: "Elaine",
-        photo: "",
+        photo: "https://upload.wikimedia.org/wikipedia/en/3/33/Elaine-benes-3707.jpg",
         scores: [
             2, 4, 3, 3, 2, 1, 3, 3, 3, 1 
         ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,43 @@
 var friendData = require("../data/friends");
 
-module.exports = function (app){
-    app.get("/api/friends", function (req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendData);
     });
+
+    app.post("/api/friends", function (req, res) {
+        var userInput = req.body;
+        var matchResult = {};
+        
+
+    
+        var maxDifference = 100;
+        var friendIndex = 0;
+    
+        for (i = 0; i < friendData.length; i++) {
+    
+            var difference = 0;
+    
+            for (j = 0; j < friendData[i].scores.length; j++) {
+                var newDifference = Math.abs(friendData[i].scores[j] - userInput.scores[j]);
+                difference += newDifference;
+            };
+    
+            if (difference < maxDifference) {
+                friendIndex = i;
+                maxDifferences = difference;
+            };
+    
+        };
+        matchResult = friendData[friendIndex];
+
+        res.json(matchResult);
+        console.log(matchResult);
+    });
 };
+
+
+
+
+
+    
